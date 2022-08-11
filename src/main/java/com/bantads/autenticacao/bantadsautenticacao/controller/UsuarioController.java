3c5cf4e47bae81e43c98fdf73f51e1c77e1c18db
Raphael Rodrigues <raphael.rodrigues@wisereducacao.com>
2022-08-11 20:46:31 -0300

    feat: inclusao de rota parcial em UsuarioController

--- src/main/java/com/bantads/autenticacao/bantadsautenticacao/controller/UsuarioController.java
+++ src/main/java/com/bantads/autenticacao/bantadsautenticacao/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
+@RequestMapping("usuarios")
 public class UsuarioController {
     
     @GetMapping("/health")
