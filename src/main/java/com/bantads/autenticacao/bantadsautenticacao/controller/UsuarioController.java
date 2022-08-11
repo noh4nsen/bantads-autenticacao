0512363a58160ecd02d14cae59eeb460074392e3
Raphael Rodrigues <raphael.rodrigues@wisereducacao.com>
2022-08-11 20:47:23 -0300

    fix: inclusao de importacao necessaria

--- src/main/java/com/bantads/autenticacao/bantadsautenticacao/controller/UsuarioController.java
+++ src/main/java/com/bantads/autenticacao/bantadsautenticacao/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.bantads.autenticacao.bantadsautenticacao.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
